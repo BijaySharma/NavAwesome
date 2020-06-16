a1b1760c6e3bdb4a4ea7a1a53824ee55204e567c
Bijay Sharma <yoursbijay@outlook.com>
2020-06-16 22:12:55 +0530

    Stack Navigation - Basic Implementation

--- App.js
+++ App.js
@@ -1,19 +1,39 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+import HomeScreen from './components/HomeScreen';
+import DetailScreen  from './components/DetailScreen';
+
+const Stack = createStackNavigator();
+
+class App extends Component {
+  
+  // This can pe passed as a prop named screenOptions to Stack Navigator
+  // screenOptions is commen options for all screens
+  screenOptions = {
+    title : 'Bijay'
+  };
+
+  render(){
+    return (
+      <NavigationContainer>
+        <Stack.Navigator initialRouteName="Home">
+          
+          <Stack.Screen 
+            name="Home" 
+            component={HomeScreen} 
+            options={{title: 'Overview'}}
+          />
+          
+          <Stack.Screen name="Details" component={DetailScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
+    );
+  }
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App;
+
+
